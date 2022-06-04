89f7b7c29ff77df4ab99bcf903582214adb72996
기웅민 <kiwoongmin@giungmin-ui-MacBookPro.local>
2022-06-04 23:09:20 +0900

    feature: show images and make create logic

--- pages/album.js
+++ pages/album.js
@@ -1,19 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useStore from "../lib/Store";
 import HeadInfo from "../components/HeadInfo";
+import AlbumBox from "../components/AlbumBox";
 
-export default function Album({ recivedList, resImage }) {
+export default function Album({ recivedList }) {
+  const [text, setText] = useState("");
   const { list, updateList } = useStore();
-  const albumList = [...list];
 
   useEffect(() => {
     updateList(recivedList);
   }, [recivedList]);
 
+  const handleCreate = (e) => {
+    setText(e.target.value)
+  };
+
+  const handleClick = (e) => {
+    list.unshift({
+      userId: list.length + 1,
+      id: list.length + 1,
+      title: text,
+    });
+
+    setText("");
+  };
+
   return (
     <div>
       <HeadInfo title="Get Album List" keyword="Album List" contents="Album List" />
       <h1>Album</h1>
+      <input type="text" name="text" value={text} onChange={handleCreate} placeholder="Write your comment" />
+      <button onClick={handleClick}>Create</button>
+
+      {list.map((item) => (
+        <div key={item.id}>
+          <AlbumBox userId={item.userId} id={item.id} title={item.title} />
+        </div>
+      ))}
     </div>
   );
 }
