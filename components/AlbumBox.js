79204ad877f6d4479233273df7fc45f7068880a0
기웅민 <kiwoongmin@giungmin-ui-MacBookPro.local>
2022-06-05 17:58:44 +0900

    feature: pagination logic

--- components/AlbumBox.js
+++ components/AlbumBox.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import useStore from "../lib/Store";
-import albumBox from "../styles/AlbumBox.module.css";
+import albumBoxStyles from "../styles/AlbumBox.module.css";
 
 export default function AlbumBox({ id, title }) {
   const { list, updateList } = useStore();
@@ -37,20 +37,20 @@ export default function AlbumBox({ id, title }) {
 
   return (
     <>
-      <div className={albumBox.images}>
-        <Image src={"https://place-hold.it/500.jpg"} alt="Image" width={100} height={100} />
+      <div className={albumBoxStyles.images}>
+        <Image src={`${process.env.NEXT_PUBLIC_IMAGE_API_URL}/500.jpg`} alt="Image" width={100} height={100} />
       </div>
       <div>{title}</div>
 
       {isClickedUpdate ?
         <>
-          <input className={albumBox.inputBox} type="text" value={text} onChange={handleUpdate} placeholder="Write new comments" />
-          <button className={albumBox.button} id={id} onClick={handleSubmitButton}>Submit</button>
+          <input className={albumBoxStyles.inputBox} type="text" value={text} onChange={handleUpdate} placeholder="Write new comments" />
+          <button className={albumBoxStyles.button} id={id} onClick={handleSubmitButton}>Submit</button>
         </>
       :
-        <button className={albumBox.button} onClick={handleUpdateButton}>Update</button>
+        <button className={albumBoxStyles.button} onClick={handleUpdateButton}>Update</button>
       }
-      <button className={albumBox.button} id={id} onClick={handleDeleteButton}>Delete</button>
+      <button className={albumBoxStyles.button} id={id} onClick={handleDeleteButton}>Delete</button>
     </>
   );
 }
