9033018fb173558dfe856fd76fb268e57b02e865
기웅민 <kiwoongmin@giungmin-ui-MacBookPro.local>
2022-06-05 15:20:19 +0900

    style: css work

--- pages/album.js
+++ pages/album.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import useStore from "../lib/Store";
 import HeadInfo from "../components/HeadInfo";
 import AlbumBox from "../components/AlbumBox";
+import album from "../styles/Album.module.css";
 
 export default function Album({ recivedList }) {
   const { list, updateList } = useStore();
@@ -30,11 +31,11 @@ export default function Album({ recivedList }) {
       <HeadInfo title="Get Album List" keyword="Album List" contents="Album List" />
       <h1>Album</h1>
 
-      <input type="text" value={text} onChange={handleCreate} placeholder="Write your comments" />
-      <button onClick={handleCreateButton}>Create</button>
+      <input className={album.inputBox} type="text" value={text} onChange={handleCreate} placeholder="Write your comments" />
+      <button className={album.button} onClick={handleCreateButton}>Create</button>
 
       {list.map((item) => (
-        <div key={item.id}>
+        <div className={album.title} key={item.id}>
           <AlbumBox id={item.id} title={item.title} />
         </div>
       ))}
